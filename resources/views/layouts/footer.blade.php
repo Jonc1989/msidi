d725c3b6570729aee976146f023e62d011b38920
Janis Mozais <janis.mozais@inbox.lv>
2016-11-12 19:25:36 +0200

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,26 @@
 <div class="col-md-12 footer">
+    <div class="row soc-icons">
+        <a href="">
+            <img src="http://ms-idi.eu/wp-content/themes/serio/images/social/round/FB.png">
+        </a>
+        <a href="">
+            <img src="http://ms-idi.eu/wp-content/themes/serio/images/social/round/Twitter.png">
+        </a>
+        <a href="">
+            <img src="http://ms-idi.eu/wp-content/themes/serio/images/social/round/Google plus.png">
+        </a>
+        <a href="">
+            <img src="http://ms-idi.eu/wp-content/themes/serio/images/social/round/Linked in.png">
+        </a>
+        <a href="">
+            <img src="http://ms-idi.eu/wp-content/themes/serio/images/social/round/youtube.png">
+        </a>
+    </div>
+
+    <div class="footer-info">
+        <div class="">info@ms-idi.eu +371 278 257 77</div>
+        <div class="">Latvia, Rezekne, Maskavas 22</div>
+        <div class="">SIA MS-IDI © 2009-<?php echo date("Y"); ?></div>
+    </div>
 
 </div>
