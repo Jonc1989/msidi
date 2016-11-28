95bcdba05393ee37478bc3694b173f2a15f9b4f9
janis.mozais@inbox.lv <janis.mozais@inbox.lv>
2016-11-28 19:57:23 +0200

    ...

--- app/Modules/CaseStudies/routes.php
+++ app/Modules/CaseStudies/routes.php
@@ -2,7 +2,17 @@
 
 Route::group(['middleware' => ['web']], function () {
 
-    Route::resource( 'case-studies', 'App\Modules\CaseStudies\Controllers\CaseStudiesController' );
+
+
+    Route::group( [ 'prefix' => '/case-studies' ], function (){
+        
+        Route::resource( '/', 'App\Modules\CaseStudies\Controllers\CaseStudiesController' );
+
+        Route::get( '/exigen', 'App\Modules\CaseStudies\Controllers\CaseStudiesController@exigen' );
+
+        Route::get( '/opal-online', 'App\Modules\CaseStudies\Controllers\CaseStudiesController@opalOnline' );
+        
+    });
 
     Route::group( [ 'prefix' => '/api' ], function ()
     {
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,9 +5,9 @@
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
         <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css">-->
-        <link rel="stylesheet" href="css/jquery.bxslider.css">
-        <link rel="stylesheet" href="css/app.css">
-        <link rel="stylesheet" href="css/ngAnimate.css">
+        <link rel="stylesheet" href="<?=  url('/') ?>/css/jquery.bxslider.css">
+        <link rel="stylesheet" href="<?=  url('/') ?>/css/app.css">
+        <link rel="stylesheet" href="<?=  url('/') ?>/css/ngAnimate.css">
     </head>
     <body>
 
@@ -28,13 +28,13 @@
 
 
 
-        <script src="js/vendor/jquery-2.2.4.min.js"></script>
-        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="<?=  url('/') ?>/js/vendor/jquery-2.2.4.min.js"></script>
+        <script src="<?=  url('/') ?>/js/vendor/bootstrap.min.js"></script>
         <script src='https://maps.googleapis.com/maps/api/js?v=3.exp'></script>
 
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>
         <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular-animate.js"></script>
-        <script src="/js/vendor/jquery.bxslider.min.js"></script>
-        <script src="/js/all.js"></script>
+        <script src="<?=  url('/') ?>/js/vendor/jquery.bxslider.min.js"></script>
+        <script src="<?=  url('/') ?>/js/all.js"></script>
     </body>
 </html>
