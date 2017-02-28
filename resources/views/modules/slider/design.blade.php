183f3e0c4c6b0919bfff1c4fb09d5048144e9cc1
Janis Mozais <janis.mozais@inbox.lv>
2017-02-28 13:26:44 +0200

    last slide changes

--- resources/views/modules/slider/design.blade.php
+++ resources/views/modules/slider/design.blade.php
@@ -2,17 +2,19 @@
 
     <div class="inner-bg">
         <div class="design-bg"></div>
-        <div class="col-lg-4 col-md-6 text-from-left white">
-            <div class="col-lg-offset-4 col-lg-8 col-md-offset-3 col-md-9">
-                <div class="title" >Product design</div>
-                <div class="title">& Development</div>
+        <div class="col-lg-4 col-md-6 text-from-left white clear-pads">
+            <div class="col-lg-12 col-md-offset-3 col-md-9 clear-pads">
+                <div class="title" >Product design & Development</div>
                 <div class="text">
                     We believe that everyone want to live in safe and well maintained environment.
                     Working closely with real estate managers and residents we can achieve results.
                     We manage all necessary data, which allows to make wise and fast decisions.
                 </div>
             </div>
-            <div class="notebook col-md-offset-5 col-md-6"></div>
+            <div class="col-md-offset-5 col-md-6">
+                <div class="notebook col-lg-12"></div>
+            </div>
+
         </div>
 
 
@@ -22,8 +24,10 @@
 
             </div>-->
 
-            <div class="col-md-12 clear-pads overflow-hidden">
-                <div class="col-md-offset-1 col-md-6 keyboard"></div>
+            <div class="col-lg-12 col-md-10 clear-pads overflow-hidden">
+                <div class="col-lg-offset-1 col-lg-6 col-md-offset-3 col-md-9 keyboard">
+
+                </div>
                 <div class="col-md-2 mouse"></div>
             </div>
             <div class="col-md-9 pens">
