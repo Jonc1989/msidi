5642e00ac200cd7cc33052d37a4e99588de206ee
Janis Mozais <janis.mozais@inbox.lv>
2017-03-10 15:33:46 +0200

    exigen 99%

--- resources/views/modules/case_studies/exigen.blade.php
+++ resources/views/modules/case_studies/exigen.blade.php
@@ -7,94 +7,118 @@
         <div  class="center short-text">Exigen Services is a private multinational company
             specializing in the software development provision services on order, and actively using the Agile methodology. During the cooperation between MS-IDI and Exigen Services, multiple projects has been successfully implemented for large clients from the USA and Europe. At the moment cooperation successfully go on and it is already the 3rd year of great cooperation.</div>
     </div>
-    <div id="exigen-info" class="col-md-12 col-sm-12 col-xs-12 clear-pads orange">
-        <div class="clear-pads">
-            <div class="medium-12 column text-center exigen-info-pic">
-                <img src="http://ms-idi.eu/wp-content/uploads/2015/05/exigen_services_logo_white.png">
-            </div>
 
-            <div class="large-10 large-offset-1 medium-12 small-12 column white left clear-pads">
-                <div class="medium-9 medium-offset-3 large-7 large-offset-3 small-12 column clear-pads">
-                    <div class="large-6 medium-6 small-6 columns exigen-info left clear-pads"> Project Manager:     </div> <div class="medium-6 small-6 columns exigen-info-text right clear-pads">Valerijs Serkins</div>
-                    <div class="large-6 medium-6 small-6 columns exigen-info left clear-pads"> Project start date:  </div> <div class="medium-6 small-6 columns exigen-info-text right clear-pads">21.01.2012</div>
-                    <div class="large-6 medium-6 small-6 columns exigen-info left clear-pads"> Project end date:    </div> <div class="medium-6 small-6 columns exigen-info-text right clear-pads"> - </div>
+    <div id="exigen-info" class="col-md-12 col-sm-12 col-xs-12 clear-pads orange">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 clear-pads">
+            <div class="orange-shit">
+                <div class="handshake">
+                    <img src="http://ms-idi.eu/wp-content//themes/serio/images/exigen.png">
+                </div>
+                <div class="">
+                    <img src="http://ms-idi.eu/wp-content/uploads/2015/05/exigen_services_logo_white.png">
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 clear-pads white ">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <div class="col-lg-4 col-md-6 col-sm-6 col-xs-6 brief-project-info pull-right">
+                    <div class=""> Project Manager:     </div>
+                    <div class=""> Project start date:  </div>
+                    <div class=""> Project end date:   </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <div class="col-lg-4 col-md-6 col-sm-6 col-xs-6 brief-project-info">
+                    <div class="">Valerijs Serkins</div>
+                    <div class="">21.01.2012</div>
+                    <div class=""> - </div>
                 </div>
             </div>
         </div>
     </div>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <div class="row block" style="margin-bottom: 5%;">
-        <h1 class="exigen-title black">Exigen Services</h1>
-        <div class="medium-6 column no-pads-right">
-            <p class="exigen-text black">Exigen Services is a private multinational company specializing in the software development provision services on order, and actively using the Agile methodology. The company is certified according to ISO 9000. The headquarters of the company is located in San Francisco, the largest software development centers are located in Riga and St. Petersburg and in other cities. Exigen Services clients are the largest global corporations and medium-sized business representers, also government agencies.</p>
-        </div>
-        <div class="medium-6 column text-center">
-            <img class="resp" src="http://ms-idi.eu/wp-content/uploads/2014/10/computer.png">
-        </div>
-    </div>
-    <div class="medium-12 column orange clear-pads">
-        <div id="coop-wrap" class="medium-6 column clear-pads">
-            <img id="coop-pic" src="http://ms-idi.eu/wp-content/uploads/2014/10/pattern.png">
-        </div>
-        <div class="row block">
-            <div class="medium-6 column">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 first-section">
+        <div class="title center title">Exigen Services</div>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <div  class="col-lg-6 pull-right">Exigen Services is a private multinational company
+                specializing in the software development provision services on order, and actively
+                using the Agile methodology. The company is certified according to ISO 9000.
+                The headquarters of the company is located in San Francisco, the largest software
+                development centers are located in Riga and St. Petersburg and in other cities.
+                Exigen Services clients are the largest global corporations and medium-sized business
+                representers, also government agencies.
             </div>
-            <div class="medium-6 column">
-                <h1 class="exigen-title white">The beginning of cooperation</h1>
-                <p class="exigen-text white">Cooperation between MS-IDI and Exigen Services began at the end of 2012, when our company started actively seeking partners, and after brief negotiations,signed a contract for participation in the first project. This contributed to a number of reasons, the main among which is the fact that the management personnel of our company in its time (early 2000’s) was successfully working in the DATI Group, which later became Exigen Services. The project associated with design and prototyping, was launched in October 2012, and was successfully submitted after 2.5 months of development, then immediately was signed a contract for mobile solution developing for the iOS platform. Then followed other projects, mainly for large customers from the USA. The spectrum of technologies used in the projects: .NET programming, mobile solution development for iOS platform, prototyping and design.</p>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 exigen-img1">
+            <img src="http://ms-idi.eu/wp-content/uploads/2014/10/computer.png">
+        </div>
+
+    </div>
+
+
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 clear-pads orange">
+        <div class="col-lg-6 col-md-6 col-sm-6 hidden-sm hidden-xs clear-pads">
+            <img  src="http://ms-idi.eu/wp-content/uploads/2014/10/pattern.png">
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 white">
+            <div class="col-lg-9 col-md-12 col-sm-12 col-xs-12" >
+                <div class="title center title">The beginning of cooperation</div>
+                <div  class="short-text">Cooperation between MS-IDI and Exigen Services began at the end of 2012, when our company started actively
+                    seeking partners, and after brief negotiations,signed a contract for participation in the first project. This contributed to a number
+                    of reasons, the main among which is the fact that the management personnel of our company in its time (early 2000’s) was successfully
+                    working in the DATI Group, which later became Exigen Services. The project associated with design and prototyping, was launched in October
+                    2012, and was successfully submitted after 2.5 months of development, then immediately was signed a contract for mobile solution developing
+                    for the iOS platform. Then followed other projects, mainly for large customers from the USA. The spectrum of technologies used in the
+                    projects: .NET programming, mobile solution development for iOS platform, prototyping and design.
+                </div>
             </div>
         </div>
     </div>
-    <div class="medium-12 column clear-pads">
-        <div id="dew-row" class="row block">
-            <div class="large-6 large-offset-0 medium-8 medium-offset-2 small-12 column">
-                <h1 class="exigen-title black">Project development</h1>
-                <p class="exigen-text black">All projects development has been implemented remotely, our developers either join to the main team, located in Riga and St. Petersburg, or create a local team working on the larger parts of the project. As practice shows a considerable distance from each other is not an obstacle for the successful implementation of projects. Currently all projects or its parts, provided for the development, were completed on time, and we have received only positive feedback from the Exigen Services side.</p>
-            </div>
-            <div class="medium-6 column">
+
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 clear-pads">
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <div class="col-lg-8 col-md-12 col-sm-12 col-xs-12 pull-right" >
+                <div class="title center title">Project development</div>
+                <div  class="short-text">All projects development has been implemented remotely, our developers either join to the main team, located in
+                    Riga and St. Petersburg, or create a local team working on the larger parts of the project. As practice shows a considerable distance
+                    from each other is not an obstacle for the successful implementation of projects. Currently all projects or its parts, provided for the
+                    development, were completed on time, and we have received only positive feedback from the Exigen Services side.
+                </div>
             </div>
         </div>
-        <div id="dev-wrap" class="large-6 large-offset-6 medium-7 column clear-pads">
-            <img id="dev-pic" src="http://ms-idi.eu/wp-content/uploads/2014/10/MAP.png">
-        </div>
-    </div>
-    <div class="medium-12 column orange">
-        <div class="row block orange">
-            <div class="medium-6 column panel">
-                <img class="exigen-pic" src="http://ms-idi.eu/wp-content/uploads/2014/10/computer1.png">
-            </div>
-            <div class="medium-6 column panel">
-                <h1 class="exigen-title white">Work</h1>
-                <p class="exigen-text white">The number of people involved in projects, varies according to demand and size of the project. The project management is carried out by Agile methodologies, particularly Scrum.a </p>
-            </div>
+        <div class="col-lg-6 col-md-6 hidden-sm hidden-xs  clear-pads">
+            <img class="pull-right" src="http://ms-idi.eu/wp-content/uploads/2014/10/MAP.png">
         </div>
     </div>
-    <div class="row block">
-        <div class="medium-12 column text-center">
-            <p class="black" style="margin-top: 5%; margin-bottom: 3%;" >At the moment the cooperation between Exigen Services and MS-IDI successfully continues and it is planned to gradually increase the number of people involved in the projects. </p>
-            <img style=" margin-bottom: 5%;" srcset="http://ms-idi.eu/wp-content/uploads/2014/10/Continue.png, http://ms-idi.eu/wp-content/uploads/2014/10/Continue@2x.png 2x">
+
+
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 clear-pads orange">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 text-right screen">
+            <img src="http://ms-idi.eu/wp-content/uploads/2014/10/computer1.png">
         </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <div class="col-md-12 col-sm-12 col-xs-12" >
+                <div class="title center title">Work</div>
+                <div  class="short-text">The number of people involved in projects, varies according to demand and size of the project.
+                    The project management is carried out by Agile methodologies, particularly Scrum.a
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center padd-top-50">
+        <div class="short-text">At the moment the cooperation between Exigen Services and
+            MS-IDI successfully continues and it is planned to gradually increase the number
+            of people involved in the projects.
+        </div>
+
+        <img style=" margin-bottom: 5%;" srcset="http://ms-idi.eu/wp-content/uploads/2014/10/Continue.png, http://ms-idi.eu/wp-content/uploads/2014/10/Continue@2x.png 2x">
     </div>
 </div>
 
